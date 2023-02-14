befa77baa17b1101c98d7f35d98775697193f723
ipmedv <125360741+ipmedv@users.noreply.github.com>
2023-02-14 15:41:01 +0300

    Исправлен файл js

--- js/js.js
+++ js/js.js
@@ -1,9 +1,3 @@
-
-// alert('WOW');
-// h1Btn.addEventListener('onclick', OnclickFunc);
-// function OnclickFunc(){
-//     alert('hello there!');  
-// }
 function btnFunc(){
     let btn = document.getElementById('btn1');
     btn.classList.toggle('btn_color');
